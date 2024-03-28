7af37d64b55f75725aa54d9810311a067296e344
Saja386 <140643092+Saja386@users.noreply.github.com>
2024-03-28 12:01:54 +0100

    feat : Cleaning Up the movemnet cpp code

--- Source/BMI_C/Player_1.cpp
+++ Source/BMI_C/Player_1.cpp
@@ -1,8 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Player_1.h"
-
 #include "BMI_CPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "Animation/BlendSpaceBase.h"
@@ -17,8 +14,9 @@ APlayer_1::APlayer_1()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
+
+
 
 // Called when the game starts or when spawned
 void APlayer_1::BeginPlay()
@@ -35,13 +33,15 @@ void APlayer_1::BeginPlay()
 	}
 	// Get character movement component 
 	PlayerCharacterMovementComponent = GetCharacterMovement() ;
-	
 }
+
+
 
 // Called every frame
 void APlayer_1::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime); 
+	Super::Tick(DeltaTime);
+	//Set player speed with checking the state of player (running / walking)
 	if (IsRunning)
 	{
 		PlayerCharacterMovementComponent -> MaxWalkSpeed = RunSpeed;
@@ -51,9 +51,9 @@ void APlayer_1::Tick(float DeltaTime)
 		PlayerCharacterMovementComponent -> MaxWalkSpeed = WalkSpeed;
 
 	}
-	
-
 }
+
+
 
 //  bind functionality to input
 void APlayer_1::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -61,30 +61,29 @@ void APlayer_1::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
 	{
-		//Binding sprint actions 
+		//Binding sprint actions
+		//WASD
 		EnhancedInputComponent->BindAction(PlayerInputMove , ETriggerEvent::Triggered , this , &APlayer_1::PlayerMove) ;
+		//mouse
 		EnhancedInputComponent->BindAction(PlayerInputLook , ETriggerEvent::Triggered , this , &APlayer_1::PlayerLook) ;
+		//Space
 		EnhancedInputComponent->BindAction(PlayerInputJump , ETriggerEvent::Started , this , &APlayer_1::PlayerJump) ;
-		
+		//Shift
 		EnhancedInputComponent->BindAction(PlayerInputDash , ETriggerEvent::Triggered , this , &APlayer_1::PlayerDash ) ; 
 	}
 }
 
-void APlayer_1::Landed(const FHitResult& Hit)
-{
-	Super::Landed(Hit);
-	FirstJump = true;
-	Jumps = 0 ; 
-}
 
 
-// Sprint 
+// Walk 
 void APlayer_1::PlayerMove(const FInputActionValue& InputValue)
 {
-	
+	//Getting direction of the player 
 	Direction = InputValue.Get<FVector2d>();
 	DirectionX = Direction.X ;
 	DirectionY = Direction.Y ;
+	
+	// check if player can run 
 	if(Direction.Y == 1)
 	{
 		IsGoingForward =  true ; 
@@ -94,19 +93,26 @@ void APlayer_1::PlayerMove(const FInputActionValue& InputValue)
 		IsGoingForward = false ;
 		IsRunning = false ;
 	}
+	
+	// Get player Velocity
 	PlayerVelocity = PlayerCharacterMovementComponent -> GetLastUpdateVelocity().Size() ;
+	
+	//Check if player stop : player cant run 
 	if (PlayerVelocity == 0 )
 	{
 		IsRunning = false ; 
 	}
+	
 	//to set extra rotation amount for dash 
 	calculateExtraRotationAmount() ;
-	// Sprint 
+	
+	// Walk function 
 	AddMovementInput(GetActorForwardVector()*Direction.Y);
 	AddMovementInput(GetActorRightVector()*Direction.X) ;
-	
 }
 
+
+// Set looking direction 
 void APlayer_1::PlayerLook(const FInputActionValue& InputValue)
 {
 	FVector2d CameraRotation = InputValue.Get<FVector2d>();
@@ -114,6 +120,9 @@ void APlayer_1::PlayerLook(const FInputActionValue& InputValue)
 	AddControllerYawInput(CameraRotation.X * CameraRotationRate);
 }
 
+
+
+// Jump function 
 void APlayer_1::PlayerJump(const FInputActionValue& InputValue)
 {
 	bool Jumped = InputValue.Get<bool>();
@@ -138,15 +147,24 @@ void APlayer_1::PlayerJump(const FInputActionValue& InputValue)
 
 
 
+// Set first Jump value when player landed in the ground
+void APlayer_1::Landed(const FHitResult& Hit)
+{
+	Super::Landed(Hit);
+	FirstJump = true;
+	Jumps = 0 ; 
+}
 
 
-// Dash 
+
+// Dash and Running state
 void APlayer_1::PlayerDash(const FInputActionValue& InputValue)
 {
 	if(IsGoingForward)
 	{
 		IsRunning = true ;
 	}
+	//Dash 
 	/*else
 	{
 		bool Dashed = InputValue.Get<bool>();
@@ -180,6 +198,10 @@ void APlayer_1::PlayerDash(const FInputActionValue& InputValue)
 	}*/
 }
 
+
+
+//Turn Gravity of for dashing 
+/*
 void APlayer_1::TurnOnGravityForDash()
 {
 	PlayerCharacterMovementComponent -> GravityScale = 1 ;
@@ -190,13 +212,18 @@ void APlayer_1::TurnOnGravityForDash()
 		PlayerCharacterMovementComponent->AddForce(PlayerVelocityForDash*ForceToStopPlayerDashing*-1) ;		
 	}
 
-}
-
+}*/
+//Dash counter
+/*
 void APlayer_1::DashDelay()
 {
 	Dashed_1 = false ;
 }
+*/
 
+
+
+// Calculating the way of moving of the player from WASD to apply extra amount of applying movement 
 void APlayer_1::calculateExtraRotationAmount()
 {
 
@@ -235,3 +262,4 @@ void APlayer_1::calculateExtraRotationAmount()
 	
 	
 }
+
