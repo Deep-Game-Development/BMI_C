88c8ad28fe821f8d8b989e412f3a5b56524ae70f
Mahyar Kazazi <rihem1386@gmail.com>
2024-04-29 20:17:32 +0330

    refactor: Upgrade Project Files to Unreal Engine 5.4
    convert project files to 5.4
    
    delete .ignore file
    
    change include name in Player_1.cpp (  BlendSpaceBase.h(14, 1): 'condition' is deprecated: BlendSpaceBase.h has been deprecated - please include and use BlendSpace.h instead. Please update your code to the new API before upgrading to the next release, otherwise your project will no longer compile. [-Wdeprecated-declarations])
    
    ignore makefile

--- Source/BMI_C/Player_1.cpp
+++ Source/BMI_C/Player_1.cpp
@@ -1,12 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "Player_1.h"
 #include "EnhancedInputSubsystems.h"
-#include "Animation/BlendSpaceBase.h"
+#include "Animation/BlendSpace.h"
 #include "EnhancedInputComponent.h"
-#include "DSP/IntegerDelay.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Kismet/GameplayStatics.h"
-#include "Physics/ImmediatePhysics/ImmediatePhysicsShared/ImmediatePhysicsCore.h"
 // Sets default values
 
 
