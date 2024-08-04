406b8ba843b93c52551980d841d01b5c7e678d24
Mahyar Kazazi <rihem1386@gmail.com>
2024-08-04 09:19:53 +0330

    feat: Add Enemy and Weapon Class Structure
    Add Enemy Classes
    Add Weapon Classes
    Add Sword Ancient Blueprints

---
+++ Source/BMI_C/Private/Enemy_Ancient.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "Enemy_Ancient.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AEnemy_Ancient : public AEnemy
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BMI_C/Private/Enemy_Ancient_Sword.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy_Ancient.h"
+#include "Enemy_Ancient_Sword.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AEnemy_Ancient_Sword : public AEnemy_Ancient
+{
+	GENERATED_BODY()
+	
+};
